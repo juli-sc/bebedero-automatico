75ed58e6cd368052a2655f7d8e0edca23cc7627c
juli-sc <juliansciammaro1342@gmail.com>
2025-02-11 20:43:34 -0300

    Add files via upload

---
+++ sensor ultrasonico.ino
@@ -0,0 +1,21 @@
+int trigPin =22; 
+int echoPin = 23;
+int tiempo=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+tiempo= pulseIn (echoPin, HIGH);
+delay(25);
+digitalWrite(trigPin, LOW);
+Serial.println(tiempo);
+}
